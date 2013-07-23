56198f86e50762aa9ed51bfda28aefcd09321a66
Arjun <arjun.aletty@gmail.com>
2013-07-23 14:16:10 -0700

    displays topDrinks only when there are drinks to display

--- brains/routes/index.js
+++ brains/routes/index.js
@@ -10,27 +10,35 @@ exports.index = function(req, res){
     var sortedOrders = topOrders(user._orders);
     console.log(sortedOrders);
     models.Drink.find().exec(function (err, drinks){
-      models.Drink.find({$or: [ {name: sortedOrders[0][0]}, {name: sortedOrders[1][0]}, {name: sortedOrders[2][0]}]}).exec(function (err, topDrinks){
-        console.log(topDrinks);
-        res.render('index', { title: 'Express', user:req.session.user, drinks:drinks, topDrinks:topDrinks});
-      })
-    })
-  })
+      if (sortedOrders.length >= 3) {
+        models.Drink.find({$or: [ {name: sortedOrders[0][0]}, {name: sortedOrders[1][0]}, {name: sortedOrders[2][0]}]}).exec(function (err, topDrinks){
+          console.log(topDrinks);
+          res.render('index', { title: 'Express', user:req.session.user, drinks:drinks, topDrinks:topDrinks});
+        });
+      } else {
+        res.render('index', {title: 'Express', user:req.session.user, drinks:drinks});
+      }
+    });
+  });
 };
 
 function topOrders(_orders) {
+  if (_orders){
     //takes name of liquid and pump number
-  var hist = {};
-  for (var i=0; i<_orders.length; i++){
-    if(!hist[_orders[i].name]){
-      hist[_orders[i].name]=1;
+    var hist = {};
+    for (var i=0; i<_orders.length; i++){
+      if(!hist[_orders[i].name]){
+        hist[_orders[i].name]=1;
+      }
+      else
+        hist[_orders[i].name]= hist[_orders[i].name]+1;
     }
-    else
-      hist[_orders[i].name]= hist[_orders[i].name]+1;
+    var sortable = [];
+    for (var drink in hist)
+      sortable.push([drink, hist[drink]])
+    sortable.sort(function(a, b) {return b[1] - a[1]})
+    return sortable;    
   }
-  var sortable = [];
-  for (var drink in hist)
-    sortable.push([drink, hist[drink]])
-  sortable.sort(function(a, b) {return b[1] - a[1]})
-  return sortable;
+  else 
+    return false;
 }
